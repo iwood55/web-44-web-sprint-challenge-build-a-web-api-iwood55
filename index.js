d673e1131062672d4a17580ec1893136baf6d238
Isaac Ellingwood <81526595+iwood55@users.noreply.github.com>
2021-08-06 16:51:13 -0400

    mvp finished

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+require("dotenv").config();
+
+const server = require("./api/server");
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(`\n*** Server running on ${port} ***\n`);
+});
 /*
 play this: https://www.youtube.com/watch?v=d-diB65scQU
 
